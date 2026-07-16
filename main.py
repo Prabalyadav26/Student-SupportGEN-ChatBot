eb532f608a28b60a0c9a3bf70ed117c396dc4a08
praval yadav <prabalyadav26@gmail.com>
2026-07-16 17:01:53 +0530

    completed project without ui

---
+++ main.py
@@ -0,0 +1,51 @@
+from retrieval.vector_store import search
+from genai.prompt import build_prompt
+from genai.gemini import ask_gemini
+from utils.chat_history import add_message
+from nlp.intent import detect_intent
+from nlp.entities import extract_entities
+
+
+while True:
+
+    query = input("\nYou : ")
+
+    if query.lower() == "exit":
+        break
+
+    intent = detect_intent(query)
+    entities = extract_entities(query)
+
+
+    if intent == "greeting":
+        print("\nBot : Hello! How can I help you today?")
+        continue
+
+    if intent == "thanks":
+        print("\nBot : You're welcome! 😊")
+        continue
+
+    results = search(query)
+
+    context = ""
+
+    for item in results:
+
+        context += f"""
+
+Question:
+{item['question']}
+
+Answer:
+{item['answer']}
+"""
+
+    prompt = build_prompt(query, context)
+
+    response = ask_gemini(prompt)
+
+    print("\nBot :", response)
+
+    add_message("user", query)
+
+    add_message("assistant", response)
